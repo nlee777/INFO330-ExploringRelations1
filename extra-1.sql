f6c784487248cb569fedbcd3476e9e438cde7e28
nlee777 <nathanlee.aos@gmail.com>
2023-04-09 19:27:27 -0700

    just need to check

--- extra-1.sql
+++ extra-1.sql
@@ -1,16 +1,6 @@
-SELECT 
-    female_doctor.first_name AS female_doctor_name, 
-    female_doctor.age AS female_doctor_age, 
-    male_reverend.first_name AS male_reverend_name, 
-    male_reverend.age AS male_reverend_age 
-FROM persons AS female_doctor 
-JOIN persons AS male_reverend 
-ON female_doctor.city = 'West Palm Beach' 
-   AND female_doctor.state = 'Florida' 
-   AND female_doctor.gender = 'female' 
-   AND female_doctor.title = 'Dr' 
-   AND male_reverend.city = 'Phoenix' 
-   AND male_reverend.state = 'Arizona' 
-   AND male_reverend.gender = 'male' 
-   AND male_reverend.title = 'Rev' 
-   AND female_doctor.age = male_reverend.age;
+SELECT * FROM persons WHERE (gender = 'Female' AND title = 'Dr' AND city = 'West Palm Beach' AND state = 'Florida')
+AND (age = (
+  SELECT age
+  FROM persons
+  WHERE (gender = 'Male' AND title = 'Rev' AND city = 'Phoenix' AND state = 'Arizona')
+));
